577e72515b6a85159d8d524032a32c405252775e
gitmagox <46257902@qq.com>
2017-08-31 21:21:15 +0800

    dingo helloworld

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 2017/8/31
+ * Time: 21:12
+ */
+
+namespace App\Http\Controllers\Api\V1;
+
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index(){
+        return "hello world";
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->get('users/index', 'App\Http\Controllers\Api\V1\UserController@index');
+});
