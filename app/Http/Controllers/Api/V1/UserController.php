a52622b411d1befc5ad220135237665fe258e19a
gitmagox <46257902@qq.com>
2017-09-04 20:01:43 +0800

    update

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -8,8 +8,10 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-
+use Tymon\JWTAuth\JWTAuth;
 use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Dingo\Api\Routing\Helpers;
 
 class UserController extends Controller
@@ -17,8 +19,25 @@ class UserController extends Controller
     use Helpers;
     public function index(){
 
-        return $this->response->error('This is an error.', 403);
-        return ['dddddd'];
+    }
+    //登陆
+    public function login( LoginRequest $request, JWTAuth $JWTAuth )
+    {
+        $credentials = $request->only(['email', 'password']);
+        try {
+            $token = $JWTAuth->attempt($credentials);
+            if(!$token) {
+                throw new AccessDeniedHttpException();
+            }
+        } catch (JWTException $e) {
+            throw new HttpException(500);
+        }
+        return response()
+                ->json([
+                    'status' => 'ok',
+                    'token' => $token
+                ]);
+
     }
 
 }
